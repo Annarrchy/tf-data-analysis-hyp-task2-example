011ac4ef3d1d95fef8ab2140558999672719b68b
Annarrchy <91114932+Annarrchy@users.noreply.github.com>
2024-05-20 16:32:30 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1063441199 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    import scipy.stats as sps
+    alpha = 0.08
+    res = sps.ks_2samp(np.ones(3), np.zeros(3))[1]
+    return res <= alpha 
